b612cc1c6619ac082063b0d60ceacb6a10bb8f2b
Prashant <prashantj121999@gmail.com>
2024-10-30 21:41:41 +0530

    ---

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ import UserPublicProfilePage from "./pages/user-public-profile.page.jsx";
 const App = () => {
   return (
     <GoogleOAuthProvider clientId={REACT_APP_GOOGLE_CLIENT_ID}>
-      <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
+      <BrowserRouter>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Layout />}>
